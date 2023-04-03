18b3e13967a5c1fa991d4b25ec815bab8042f41b
ofemjohn <ofemjohn@gmail.com>
2023-04-03 13:25:12 +0100

    using the random.uniform method to delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,6 @@ import random
     asynchronous coroutine that takes in an integer argument
 '''
 
-
 async def wait_random(max_delay=10):
     '''awaites a random number from 0 -10'''
     wait = random.uniform(0, max_delay)
