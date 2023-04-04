ebf87fd9981c115467be11e56f7ac9df1e62536c
ofemjohn <ofemjohn@gmail.com>
2023-04-04 22:06:34 +0100

    run time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Run a comprehension asynchronously"""
+
+from asyncio import gather
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ measure_runtime should measure the total runtime and return it. """
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
