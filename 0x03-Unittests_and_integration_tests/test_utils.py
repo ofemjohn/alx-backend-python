f3144e42d3344567e4c1bab31f8518108b321e12
ofemjohn <ofemjohn@gmail.com>
2023-04-24 19:47:41 +0100

    added a test case for assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,7 +12,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, nested_map, path, expected_outcome):
+    def test_access_nested_map(self, nested_map, path, expected_output):
         '''test the nested map with the path'''
         self.assertEqual(utils.access_nested_map(nested_map, path),
-                         expected_outcome)
+                         expected_output)
+
+    @parameterized.expand([
+        ({}, ('a',), "KeyError('a',)"),
+        ({'a' : 1}, ({'a' : 'b'}), "KeyError('b',)")])
+
+    def test_access_nested_map_exception(self, nested_map, path, expected_excepeption):
+        '''test map with exception thrown'''
+        with self.assertRaises(KeyError) as key_error:
+            utils.access_nested_map(nested_map, map, expected_excepeption)
